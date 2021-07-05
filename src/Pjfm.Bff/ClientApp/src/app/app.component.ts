21115ec097246f3cd7799296729497d5153ed632
Meindert <meindertvanduin99@gmail.com>
2021-07-05 23:31:09 +0200

    Add frontend with linter and prettier

---
+++ src/Pjfm.Bff/ClientApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pjfm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ClientApp';
+}
---
+++ src/Pjfm.Bff/ClientApp/src/main.ts
@@ -0,0 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(() => {
+    // TODO: add some way of notifying the user that the application could not start
+  });
