2674d5301ea968bddb0e35e42e25d8417a27e168
Meindert <meindertvanduin99@gmail.com>
2021-07-06 21:14:46 +0200

    Add api project with an simple Gebruikers domain

---
+++ src/Pjfm.Common/Guard.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Pjfm.Common
+{
+    public static class Guard
+    {
+        public static T NotNull<T>(T? value, string name) where T : class
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return value;
+        }
+
+        public static string NotNullOrEmpty(string? value, string name)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Value can't be null or an empty string", name);
+            }
+
+            return value;
+        }
+        
+        public static Guid NotEmpty(Guid value, string name)
+        {
+            if (value == Guid.Empty)
+            {
+                throw new ArgumentException("Value can't be an empty GUID", name);
+            }
+
+            return value;
+        }
+    }
+}
