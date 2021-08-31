65bfecd0441cf7775b796baf6a6c140b13679d2b
Meindert <meindertvanduin99@gmail.com>
2021-08-31 22:09:05 +0200

    Add session information at top of page

---
+++ src/Pjfm.Bff/ClientApp/src/app/modules/session/components/ascii-slider/ascii-slider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pjfm-ascii-slider',
+  templateUrl: './ascii-slider.component.html',
+  styleUrls: ['./ascii-slider.component.scss'],
+})
+export class AsciiSliderComponent {
+  
+}
--- src/Pjfm.Bff/ClientApp/src/app/modules/session/session.module.ts
+++ src/Pjfm.Bff/ClientApp/src/app/modules/session/session.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionRoutingModule } from './session-routing.module';
 import { SessionComponent } from './features/session.component';
+import { AsciiSliderComponent } from './components/ascii-slider/ascii-slider.component';
 
 @NgModule({
-  declarations: [SessionComponent],
+  declarations: [SessionComponent, AsciiSliderComponent],
   imports: [CommonModule, SessionRoutingModule],
 })
 export class SessionModule {}
