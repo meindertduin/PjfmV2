e4374e3de4b549c5718b4c8ebca63f48d0fe1d09
Meindert <meindertvanduin99@gmail.com>
2021-09-01 19:54:10 +0200

    Added responsibility to the session page

--- src/Pjfm.Bff/ClientApp/src/app/modules/session/session.module.ts
+++ src/Pjfm.Bff/ClientApp/src/app/modules/session/session.module.ts
@@ -5,9 +5,16 @@ import { SessionComponent } from './features/session.component';
 import { AsciiSliderComponent } from './components/ascii-slider/ascii-slider.component';
 import { TrackProgressionBarComponent } from './components/track-progression-bar/track-progression-bar.component';
 import { ConvertMsToTimePipe } from './pipes/convert-ms-to-time.pipe';
+import { InlineSVGModule } from 'ng-inline-svg';
+import { HttpClientModule } from '@angular/common/http';
+import { QueuedTrackComponent } from './components/queued-track/queued-track.component';
+import { PlaybackButtonsComponent } from './components/playback-buttons/playback-buttons.component';
+
+const COMPONENTS = [SessionComponent, AsciiSliderComponent, TrackProgressionBarComponent];
+const PIPES = [ConvertMsToTimePipe];
 
 @NgModule({
-  declarations: [SessionComponent, AsciiSliderComponent, TrackProgressionBarComponent, ConvertMsToTimePipe],
-  imports: [CommonModule, SessionRoutingModule],
+  declarations: [COMPONENTS, PIPES, QueuedTrackComponent, PlaybackButtonsComponent],
+  imports: [CommonModule, SessionRoutingModule, InlineSVGModule, HttpClientModule],
 })
 export class SessionModule {}
