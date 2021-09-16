0e1bbdd29302f00a232fdfa175a4cfc43c28bf21
Meindert <meindertvanduin99@gmail.com>
2021-09-16 21:40:27 +0200

    Finish the dropdown component

--- src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/form-inputs.module.ts
+++ src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/form-inputs.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DropDownComponent } from './drop-down/drop-down.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SelectComponent } from './select/select.component';
 import { SelectOptionComponent } from './select/select-option/select-option.component';
 
 @NgModule({
-  declarations: [DropDownComponent, SelectComponent, SelectOptionComponent],
-  exports: [DropDownComponent, SelectComponent, SelectOptionComponent],
+  declarations: [SelectComponent, SelectOptionComponent],
+  exports: [SelectComponent, SelectOptionComponent],
   imports: [CommonModule, ReactiveFormsModule],
 })
 export class FormInputsModule {}
