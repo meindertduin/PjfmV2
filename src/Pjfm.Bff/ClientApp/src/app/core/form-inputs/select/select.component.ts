9f128229f2d6811db738124ddc440f39056e2543
Meindert <meindertvanduin99@gmail.com>
2021-09-16 21:02:27 +0200

    Added value acceser for ngModel for select component

--- src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/select/select.component.ts
+++ src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/select/select.component.ts
@@ -1,16 +1,35 @@
-import { AfterViewInit, Component, ContentChildren, Input, OnDestroy, QueryList } from '@angular/core';
+import { AfterViewInit, Component, ContentChildren, forwardRef, Input, OnDestroy, QueryList } from '@angular/core';
 import { SelectOptionComponent } from './select-option/select-option.component';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'pjfm-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
 })
-export class SelectComponent implements AfterViewInit, OnDestroy {
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/no-empty-function, @typescript-eslint/explicit-module-boundary-types */
+export class SelectComponent implements AfterViewInit, OnDestroy, ControlValueAccessor {
   showOptions = false;
   textValue?: string;
+
+  value: any;
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  onChange = (_: any) => {};
+  onTouched = () => {};
+
+  setValue(value: any) {
+    this.value = value;
+    this.onChange(this.value);
+  }
 
   @Input() placeHolder = '';
   @ContentChildren(SelectOptionComponent) options!: QueryList<SelectOptionComponent>;
@@ -26,6 +45,7 @@ export class SelectComponent implements AfterViewInit, OnDestroy {
       option.clickEvent.pipe(takeUntil(this._destroyed$)).subscribe((event) => {
         this.textValue = event.textValue;
         this.showOptions = false;
+        this.setValue(event.value);
       });
     });
   }
@@ -33,5 +53,17 @@ export class SelectComponent implements AfterViewInit, OnDestroy {
   ngOnDestroy(): void {
     this._destroyed$.complete();
     this._destroyed$.next();
+  }
+
+  registerOnChange(fn: (_: any) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: any): void {
+    this.setValue(value);
   }
 }
--- src/Pjfm.Bff/ClientApp/src/app/modules/home/home.module.ts
+++ src/Pjfm.Bff/ClientApp/src/app/modules/home/home.module.ts
@@ -5,12 +5,13 @@ import { HomeRoutingModule } from './home-routing.module';
 import { PlaybackGroupComponent } from './components/playbackgroup/playback-group.component';
 import { SharedModule } from '../../shared/shared.module';
 import { FormInputsModule } from '../../core/form-inputs/form-inputs.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const COMPONENTS = [HomeComponent, PlaybackGroupComponent];
 
 @NgModule({
   declarations: [COMPONENTS],
-  imports: [CommonModule, HomeRoutingModule, SharedModule, FormInputsModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, FormInputsModule, ReactiveFormsModule, FormsModule],
   providers: [],
 })
 export class HomeModule {}
