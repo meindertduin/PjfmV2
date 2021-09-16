58db716558d5228587e80879bc4bc332cbd82947
Meindert <meindertvanduin99@gmail.com>
2021-09-16 20:04:28 +0200

    Add functionality to select for selecting new values

--- src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/select/select-option/select-option.component.ts
+++ src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/select/select-option/select-option.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'pjfm-select-option',
   templateUrl: './select-option.component.html',
   styleUrls: ['./select-option.component.scss'],
 })
-export class SelectOptionComponent {
+export class SelectOptionComponent implements AfterViewInit {
   @Input() value: unknown;
+  clickEvent = new EventEmitter<SelectClickEvent>();
+
+  private textValue!: string;
+
+  constructor(private readonly _elementRef: ElementRef) {}
+
+  onClick(): void {
+    this.clickEvent.emit({
+      value: this.value,
+      textValue: this.textValue,
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const innerText = this._elementRef.nativeElement.innerText as string | undefined;
+    if (innerText == null) {
+      this.textValue = '';
+    } else {
+      this.textValue = innerText;
+    }
+  }
+}
+
+export interface SelectClickEvent {
+  value: unknown;
+  textValue: string;
 }
--- src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/select/select.component.ts
+++ src/Pjfm.Bff/ClientApp/src/app/core/form-inputs/select/select.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ContentChildren, Input, OnDestroy, QueryList } from '@angular/core';
+import { SelectOptionComponent } from './select-option/select-option.component';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'pjfm-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
 })
-export class SelectComponent {
+export class SelectComponent implements AfterViewInit, OnDestroy {
   showOptions = false;
+  textValue?: string;
+
+  @Input() placeHolder = '';
+  @ContentChildren(SelectOptionComponent) options!: QueryList<SelectOptionComponent>;
+
+  private readonly _destroyed$ = new Subject();
 
   onSelectClick(): void {
     this.showOptions = !this.showOptions;
+  }
+
+  ngAfterViewInit(): void {
+    this.options.toArray().forEach((option) => {
+      option.clickEvent.pipe(takeUntil(this._destroyed$)).subscribe((event) => {
+        this.textValue = event.textValue;
+        this.showOptions = false;
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.complete();
+    this._destroyed$.next();
   }
 }
