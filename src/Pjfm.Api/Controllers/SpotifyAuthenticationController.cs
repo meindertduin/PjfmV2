9177b39ceceb37f46e6040253ced6b2c57cee006
Meindert <meindertvanduin99@gmail.com>
2021-09-20 21:20:27 +0200

    Validate state when authenticating spotify

--- src/Pjfm.Api/Controllers/SpotifyAuthenticationController.cs
+++ src/Pjfm.Api/Controllers/SpotifyAuthenticationController.cs
@@ -6,11 +6,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Pjfm.Api.Authentication;
 using Pjfm.Api.Controllers.Base;
 using Pjfm.Application.Authentication;
 using Pjfm.Application.GebruikerNummer;
 using Pjfm.Application.Spotify;
-using Pjfm.Common.Extensions;
 
 namespace Pjfm.Api.Controllers
 {
@@ -23,6 +23,7 @@ namespace Pjfm.Api.Controllers
         private readonly IUserTokenService _userTokenService;
         private readonly IApplicationUserRepository _applicationUserRepository;
         private readonly ISpotifyTrackService _spotifyTrackService;
+        private readonly StateValidator _stateValidator;
 
         public SpotifyAuthenticationController(IPjfmControllerContext pjfmContext,
             ISpotifyAuthenticationService spotifyAuthenticationService,
@@ -36,16 +37,20 @@ namespace Pjfm.Api.Controllers
             _userTokenService = userTokenService;
             _applicationUserRepository = applicationUserRepository;
             _spotifyTrackService = spotifyTrackService;
+
+            _stateValidator = new StateValidator();
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status302Found)]
         public IActionResult Authenticate()
         {
+            var state = _stateValidator.GenerateNewState();
+
             var authorizationUrl = new StringBuilder("https://accounts.spotify.com/authorize")
                 .Append($"?client_id={Configuration.GetValue<string>("Spotify:ClientId")}")
                 .Append("&response_type=code")
-                .Append($"&state={Helpers.RandomString(30)}")
+                .Append($"&state={state}")
                 .Append($@"&redirect_uri={Configuration.GetValue<string>("Spotify:RedirectUrl")}")
                 .Append(
                     "&scope=user-top-read user-read-private streaming user-read-playback-state playlist-read-private playlist-read-collaborative")
@@ -55,18 +60,23 @@ namespace Pjfm.Api.Controllers
         }
 
         [HttpGet("callback")]
-        [ProducesResponseType(typeof(string),StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
         public async Task<IActionResult> Callback([FromQuery] string state, [FromQuery] string code)
         {
-            // TODO: add validation for the code
+            if (!_stateValidator.ValidateState(state))
+            {
+                return BadRequest("Could not validate state.");
+            }
 
             var requestResult = await _spotifyAuthenticationService.RequestAccessToken(code);
             if (requestResult.IsSuccessful)
             {
-                await _spotifyUserDataRepository.SetUserRefreshToken(PjfmPrincipal.Id ,requestResult.Result.RefreshToken);
+                await _spotifyUserDataRepository.SetUserRefreshToken(PjfmPrincipal.Id,
+                    requestResult.Result.RefreshToken);
                 await _applicationUserRepository.SetUserSpotifyAuthenticated(PjfmPrincipal.Id, true);
-                
-                _userTokenService.StoreUserSpotifyAccessToken(PjfmPrincipal.Id, requestResult.Result.AccessToken, requestResult.Result.ExpiresIn);
+
+                _userTokenService.StoreUserSpotifyAccessToken(PjfmPrincipal.Id, requestResult.Result.AccessToken,
+                    requestResult.Result.ExpiresIn);
                 await _spotifyTrackService.SetUserSpotifyTracks(PjfmPrincipal.Id);
             }
 
