7b77b074cefa5147015150927027193de8e324ab
Meindert <meindertvanduin99@gmail.com>
2021-09-21 19:14:44 +0200

    Fix tests and linting

--- src/Pjfm.Bff/ClientApp/src/app/shared/services/user.service.spec.ts
+++ src/Pjfm.Bff/ClientApp/src/app/shared/services/user.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
+import { UserClient } from '../../core/services/api-client.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [UserClient],
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(UserService);
   });
 
