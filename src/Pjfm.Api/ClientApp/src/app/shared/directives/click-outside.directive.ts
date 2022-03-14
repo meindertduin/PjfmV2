54bfe794e1c951ada423123423bbe9fc63218ce0
Meindert <meindertvanduin99@gmail.com>
2022-03-14 19:05:26 +0100

    convert click outside logic to where the click must be outside the geometric coords

--- src/Pjfm.Api/ClientApp/src/app/shared/directives/click-outside.directive.ts
+++ src/Pjfm.Api/ClientApp/src/app/shared/directives/click-outside.directive.ts
@@ -4,21 +4,55 @@ import { Directive, EventEmitter, ElementRef, Output, HostListener } from '@angu
   selector: '[pjfmClickOutside]',
 })
 export class ClickOutsideDirective {
-  private _clickCount = 0;
   @Output() pjfmClickOutside = new EventEmitter();
   constructor(private readonly _elementRef: ElementRef) {}
 
-  @HostListener('document:click', ['$event.target'])
+  @HostListener('document:click', ['$event'])
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  onClick(target: any) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
-    const clickInside: boolean = this._elementRef.nativeElement.contains(target);
+  onClick(event: MouseEvent) {
+    const targetElement = this._elementRef.nativeElement as HTMLElement;
+    const width = targetElement.offsetWidth;
+    const height = targetElement.offsetHeight;
+
+    const divX = this.findPosX(targetElement);
+    const divY = this.findPosY(targetElement);
+
+    const clickX = Math.abs(event.x - divX) * 2;
+    const clickY = event.y - divY;
+
+    const clickInside: boolean = clickX >= 0 && clickX <= width && clickY >= 0 && clickY <= height;
     if (!clickInside) {
-      // One click outside is permitted for dialogs, as buttons are clicked outside the direcftive component to activate the dialog
-      if (this._clickCount > 0) {
-        this.pjfmClickOutside.emit();
-      }
-      this._clickCount++;
+      this.pjfmClickOutside.emit();
     }
   }
+
+  /* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any */
+  findPosX(obj: any): number {
+    let curleft = 0;
+    if (obj.offsetParent) {
+      while (obj.offsetParent) {
+        curleft += obj.offsetLeft;
+        obj = obj.offsetParent;
+      }
+    } else if (obj.x) {
+      curleft += obj.x;
+    }
+    return curleft;
+  }
+
+  findPosY(obj: any): number {
+    let curtop = 0;
+    if (obj.offsetParent) {
+      while (obj.offsetParent) {
+        curtop += obj.offsetTop;
+        obj = obj.offsetParent;
+      }
+    } else if (obj.y) {
+      curtop += obj.y;
+    }
+
+    return curtop;
+  }
+
+  /* eslint-enable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any  */
 }
