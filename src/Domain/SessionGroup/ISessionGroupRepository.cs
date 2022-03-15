140984007cace868614134a933b6628d1afe2c04
Meindert <meindertvanduin99@gmail.com>
2022-03-15 16:48:49 +0100

    Add repository functions for handling SessionGroups in the database

--- src/Domain/SessionGroup/ISessionGroupRepository.cs
+++ src/Domain/SessionGroup/ISessionGroupRepository.cs
@@ -1,6 +1,21 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
 namespace Domain.SessionGroup
 {
     public interface ISessionGroupRepository
     {
+        /// <summary>
+        /// Creates a SessionGroup in the Database.
+        /// </summary>
+        /// <param name="groupName">The name of the new SessionGroup</param>
+        /// <exception cref="DuplicateNameException">
+        ///     A SessionGroup with the given groupName already exists.
+        /// </exception>
+        Task CreateSessionGroup(string groupName);
+        IEnumerable<SessionGroup> GetAllSessionGroups();
+        Task AddFillerQueueParticipant(string groupId, ApplicationUser.ApplicationUser user);
+        Task RemoveFillerQueueParticipant(string groupId, ApplicationUser.ApplicationUser user);
     }
 }
--- src/Pjfm.Infrastructure/Repositories/SessionGroupRepository.cs
+++ src/Pjfm.Infrastructure/Repositories/SessionGroupRepository.cs
@@ -1,19 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Threading.Tasks;
 using Domain.ApplicationUser;
 using Domain.SessionGroup;
+using Microsoft.EntityFrameworkCore;
+using Pjfm.Common;
 
 namespace Pjfm.Infrastructure.Repositories
 {
     public class SessionGroupRepository : ISessionGroupRepository
     {
-        // public Task AddFillerQueueParticipant(ApplicationUser user)
-        // {
-        //     
-        // }
+        private readonly PjfmContext _pjfmContext;
 
-        // public Task RemoveFillerQueueParticipant(ApplicationUser user)
-        // {
-        //     
-        // }
+        public SessionGroupRepository(PjfmContext pjfmContext)
+        {
+            _pjfmContext = pjfmContext;
+        }
+
+        public Task CreateSessionGroup(string groupName)
+        {
+            var group = _pjfmContext.SessionGroups.FirstOrDefault(s => s.GroupName == groupName);
+
+            if (group == null)
+            {
+                throw new DuplicateNameException($"A SessionGroup with name: {groupName} already exists.");
+            }
+
+            _pjfmContext.SessionGroups.Add(new SessionGroup()
+            {
+                GroupName = groupName,
+                Id = Guid.NewGuid().ToString(),
+                FillerQueueParticipants = new List<ApplicationUser>(),
+            });
+
+            return _pjfmContext.SaveChangesAsync();
+        }
+
+        public IEnumerable<SessionGroup> GetAllSessionGroups()
+        {
+            return _pjfmContext.SessionGroups.AsNoTracking().ToList();
+        }
+        
+        public Task AddFillerQueueParticipant(string groupId, ApplicationUser user)
+        {
+            var group = _pjfmContext.SessionGroups.AsNoTracking().FirstOrDefault(g => g.Id == groupId);
+            Guard.NotNull(group, nameof(group));
+
+            group!.FillerQueueParticipants.Add(user);
+            return _pjfmContext.SaveChangesAsync();
+        }
+
+        public Task RemoveFillerQueueParticipant(string groupId, ApplicationUser user)
+        {
+            var group = _pjfmContext.SessionGroups.AsNoTracking().FirstOrDefault(g => g.Id == groupId);
+            Guard.NotNull(group, nameof(group));
+
+            group!.FillerQueueParticipants.Remove(user);
+            return _pjfmContext.SaveChangesAsync();
+        }
     }
 }
