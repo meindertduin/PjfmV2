b33e754976ca42202b44ee450910f18458f6ef7d
Meindert <meindertvanduin99@gmail.com>
2022-06-22 17:38:36 +0200

    Fix global scss for svelte

--- src/Pjfm.Api/ClientApp/rollup.config.js
+++ src/Pjfm.Api/ClientApp/rollup.config.js
@@ -50,7 +50,7 @@ export default {
 		// we'll extract any component CSS out into
 		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
-		scss({ in})
+		scss({ output: 'public/build/styles.css', includePaths: ['./scss/styles.scss'] }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
